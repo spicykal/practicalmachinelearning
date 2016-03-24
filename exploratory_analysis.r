4fabc8e235e4161b55a57f8dc30073373772748a
spicykal <spicykal@gmail.com>
2016-03-24 10:19:00 -0400

    Update exploratory_analysis.r

--- exploratory_analysis.r
+++ exploratory_analysis.r
@@ -9,3 +9,18 @@ train$X <- NULL
 
 ## remove the columns with 19,216 NA observations
 train <- train[,colSums(is.na(train)) < 19216]
+
+## create a variable to represent the x-axis as "classe" is a factor and
+## histograms can only be drawn for numeric data
+classnum <- as.numeric(train$classe)
+
+## create the histogram and change its density to percentages
+h <- hist(classnum)
+h$density <- h$counts / sum(h$counts) * 100
+
+## plot the histogram, leaving the x-axis blank
+plot(h, freq = F, main = "Histogram of Classe for the Training Set", ylab = 
+         "Percentage", xlab="Classe", xaxt="n", col="blue")
+
+## add the axis
+axis(1,at=1:5,labels = c("A","B","C","D","E"))
