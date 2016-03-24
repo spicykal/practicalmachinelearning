e5b7007ea03e11109d1167d03cdd0929242d0e7a
spicykal <spicykal@gmail.com>
2016-03-24 10:18:17 -0400

    Update exploratory_analysis.r

--- exploratory_analysis.r
+++ exploratory_analysis.r
@@ -3,3 +3,9 @@ summ <- summary(train)
 
 ## use the R2HTML package's "HTML" function to write the output to an html file
 HTML(summ, file="summary.html")
+
+## remove the column with the row number
+train$X <- NULL
+
+## remove the columns with 19,216 NA observations
+train <- train[,colSums(is.na(train)) < 19216]
